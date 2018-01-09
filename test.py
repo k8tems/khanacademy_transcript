981e6d7c8d98884cb8b39a935b4b3d09c74d56d6
k8tems <k8tems@live.com>
2018-01-10 07:51:51 +0900

    Adjust test

--- test.py
+++ test.py
@@ -6,11 +6,11 @@ class TestExtractTutorials(unittest.TestCase):
     def test(self):
         with open('fixture.html') as f:
             data = f.read()
-        self.assertEqual('foo', main.extract_tutorials(data))
+        self.assertEqual({'loggedIn': True, 'componentProps': {'foo': 'bar'}}, main.extract_react_component(data))
 
     def test_component_not_found(self):
         data = '<script></script>'
-        self.assertRaises(main.ReactComponentNotFound, main.extract_tutorials, data)
+        self.assertRaises(main.ReactComponentNotFound, main.extract_react_component, data)
 
 
 if __name__ == '__main__':
