0e38de3cbe8ed791ad8a27a4b9a596be1d3514d9
k8tems <k8tems@live.com>
2018-01-10 07:37:26 +0900

    Add test for component not found

--- test.py
+++ test.py
@@ -8,6 +8,10 @@ class TestParseTutorials(unittest.TestCase):
             data = f.read()
         self.assertEqual('foo', main.parse_tutorials(data))
 
+    def test_component_not_found(self):
+        data = '<script></script>'
+        self.assertRaises(main.ReactComponentNotFound, main.parse_tutorials, data)
+
 
 if __name__ == '__main__':
     unittest.main()
