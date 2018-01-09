15d1c6aca0992d2de5ea188e69a362b320cb1f7d
k8tems <k8tems@live.com>
2018-01-10 07:34:25 +0900

    Fix test

--- test.py
+++ test.py
@@ -6,8 +6,7 @@ class TestParseTutorials(unittest.TestCase):
     def test(self):
         with open('fixture.html') as f:
             data = f.read()
-        expected = {'componentProps': [{'foo': 'bar'}]}
-        self.assertEqual(expected, main.parse_tutorials(data))
+        self.assertEqual('foo', main.parse_tutorials(data))
 
 
 if __name__ == '__main__':
