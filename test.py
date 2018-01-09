6bf6a7fbca86d7146b41177d8fc09c4b4ba3e961
k8tems <k8tems@live.com>
2018-01-10 08:14:40 +0900

    Rename script

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 import unittest
-import main
+import download_tutorials
 
 
 class TestExtractReactComponent(unittest.TestCase):
     def test(self):
         with open('fixture.html') as f:
             data = f.read()
-        self.assertEqual({'loggedIn': True, 'componentProps': {'foo': 'bar'}}, main.extract_react_component(data))
+        self.assertEqual({'loggedIn': True, 'componentProps': {'foo': 'bar'}}, download_tutorials.extract_react_component(data))
 
     def test_component_not_found(self):
         data = '<script></script>'
-        self.assertRaises(main.ReactComponentNotFound, main.extract_react_component, data)
+        self.assertRaises(download_tutorials.ReactComponentNotFound, download_tutorials.extract_react_component, data)
 
 
 if __name__ == '__main__':
