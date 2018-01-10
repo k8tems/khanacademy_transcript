c37ec98a15db672f3d138eb16683d106b3f7bbf3
k8tems <k8tems@live.com>
2018-01-10 09:24:52 +0900

    Add failing test for parsing transcript

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import download_tutorials
+import parse_transcripts
 
 
 class TestExtractReactComponent(unittest.TestCase):
@@ -12,6 +13,14 @@ class TestExtractReactComponent(unittest.TestCase):
     def test_component_not_found(self):
         data = '<script></script>'
         self.assertRaises(download_tutorials.ReactComponentNotFound, download_tutorials.extract_react_component, data)
+
+
+class TestParseTranscript(unittest.TestCase):
+    def test(self):
+        transcript = '<?xml version="1.0" encoding="utf-8" ?><transcript>' \
+                     '<text start="0" dur="0.83">foo</text><text start="0.83" dur="5.01">bar</text></transcript>'
+        expected = '0    foo\n0.83  bar'
+        self.assertEqual(expected, parse_transcripts.parse_transcript(transcript))
 
 
 if __name__ == '__main__':
