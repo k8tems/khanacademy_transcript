fd85d798d169b883ee66bb4c5dcf75627d26870b
k8tems <k8tems@live.com>
2018-01-10 09:37:07 +0900

    Add method to parse transcript

--- parse_transcripts.py
+++ parse_transcripts.py
@@ -1,6 +1,15 @@
 import os
 
 
+def read_text(fname):
+    with open(fname) as f:
+        return f.read()
+
+
+def parse_transcript(xml_transcript):
+    pass
+
+
 if __name__ == '__main__':
     xml_dir = os.path.join('transcripts', 'xml')
     module_dirs = os.listdir(xml_dir)
@@ -9,3 +18,8 @@ if __name__ == '__main__':
         content_dirs = os.listdir(os.path.join(xml_dir, md))
         for cd in content_dirs:
             print('\t', cd)
+            tutorials = os.listdir(os.path.join(xml_dir, md, cd))
+            for t in tutorials:
+                print('\t\t', t)
+                xml_transcript = read_text(os.path.join(xml_dir, md, cd, t))
+                parse_transcript(xml_transcript)
